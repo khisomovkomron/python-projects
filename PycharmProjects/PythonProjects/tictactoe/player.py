72b3a550bd60b93f18215cf872fa310b6f025e27
khisomovkomron <normok_9595@bk.ru>
2020-12-29 19:22:15 +0300

    adding tictactoe game

---
+++ PycharmProjects/PythonProjects/tictactoe/player.py
@@ -0,0 +1,40 @@
+import math
+import random
+# import game
+
+class Player:
+    def __init__(self, letter):
+        # letter is x or o
+        self.letter = letter
+
+    # we want all players to get their next move given a game
+    def get_move(self, game):
+        pass
+
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        square = random.choice(game.available_move())
+        return square
+
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (0-8):')
+            try:
+                val = int(square)
+                if val not in game.available_move():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print('Invalid square. Try again')
+        return val
